755d001d08700a5efe50fb70298dfc3a7806229a
MOULI PATHALA <moulipathala@gmail.com>
2024-11-10 13:17:48 +0530

    feature-300 changes

--- src/main/java/in/ashokit/SpringbootWithGithubDemoApplication.java
+++ src/main/java/in/ashokit/SpringbootWithGithubDemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootWithGithubDemoApplication {
 
+      private static final String rest_url="https://www.equifax.com/getscores";    
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootWithGithubDemoApplication.class, args);
 		int i=10;
