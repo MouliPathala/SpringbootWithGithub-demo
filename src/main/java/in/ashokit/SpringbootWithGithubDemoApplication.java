19a79461128efb8d168c915b36fbb762f08bf897
MOULI PATHALA <moulipathala@gmail.com>
2024-11-10 07:41:33 +0530

    variable j is added

--- src/main/java/in/ashokit/SpringbootWithGithubDemoApplication.java
+++ src/main/java/in/ashokit/SpringbootWithGithubDemoApplication.java
@@ -9,6 +9,7 @@ public class SpringbootWithGithubDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootWithGithubDemoApplication.class, args);
 		int i=10;
+                int j=20;
 	}
 
 }
