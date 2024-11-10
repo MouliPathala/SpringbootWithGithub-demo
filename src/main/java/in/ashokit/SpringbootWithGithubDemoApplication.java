e6d65deaa537c3face2f0e2a44348ddf0de3162c
MOULI PATHALA <moulipathala@gmail.com>
2024-11-10 13:26:59 +0530

    feature-301 changes

--- src/main/java/in/ashokit/SpringbootWithGithubDemoApplication.java
+++ src/main/java/in/ashokit/SpringbootWithGithubDemoApplication.java
@@ -6,10 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootWithGithubDemoApplication {
 
+        private Map<String,Object> cache=new HashMap<String,Object>();
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootWithGithubDemoApplication.class, args);
 		int i=10;
                 int j=20;
 	}
+
+        public void lodaDataToCache(){
+
+               //logic
+        }
 
 }
